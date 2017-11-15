6ffa496f3cb64a230d5aa04939f10c6b26b49e96
Lazovchik <33687555+Lazovchik@users.noreply.github.com>
2017-11-15 11:49:23 +0100

    Update Card.hpp

--- Card.hpp
+++ Card.hpp
@@ -19,7 +19,7 @@ private:
     bool m_invoked;
     bool m_alive;
     
-    Ressource m_ressources;//pour les carte-terrain
+    Ressource* m_ressources;//pour les carte-terrain
     Static_Parameters m_s_parametrs;//nom, image, cout etc..
     Variant_Parametrs* m_v_parametres;//la vie, la force
     Sp_at_par * m_sp_at_parameter;//attaque speciale
@@ -27,7 +27,17 @@ private:
     //Accees au tableau des energies A FAIRE
     
 public:
+    Card();
+    ~Card();
     
+    bool Get_hand_status();
+    bool Get_invoke_status();
+    bool Get_live_status();
+    
+    void Set_hand_status (bool var);
+    void Set_invoke_status (bool var);
+    void Set_live_status (bool var);
+
     
     
     
