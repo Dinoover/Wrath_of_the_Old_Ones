c5c8a9e79c08dcb34eb4ddb11b566b71a3f167a9
Lazovchik <33687555+Lazovchik@users.noreply.github.com>
2017-11-21 09:54:12 +0100

    Update Sub_classes.cpp

--- Sub_classes.cpp
+++ Sub_classes.cpp
@@ -8,39 +8,54 @@
 
 #include "Sub_classes.hpp"
 
-Ressource::Get_blood()
+Ressource:: Ressource()
+{
+    
+}
+
+Ressource:: Ressource (int blood, int gold, int power, int influence)
+{
+    *m_blood=blood;
+    *m_gold=gold;
+    *m_power=power;
+    *m_influence=influence;
+    
+}
+
+Ressource:: ~Ressource()
+{
+    
+}
+
+ int Ressource:: Get_blood()
 {
     return *m_blood;
 }
-
-Ressource::Get_gold()
+ int Ressource:: Get_gold()
 {
-    return *m_gold;
+    return * m_gold;
 }
-
-Ressource::Get_power()
+int Ressource:: Get_power()
 {
     return *m_power;
 }
-
-Ressource::Get_influence()
+int Ressource::Get_influence()
 {
     return *m_influence;
 }
 
-void Ressource::Set_blood(int blood)
-{
-    *m_blood=blood;
-}
 
-void Ressource::Set_gold(int gold)
+void Ressource:: Set_blood(int var)
 {
-    *m_gold=gold;
+    *m_blood=var;
 }
-
-void Ressource::Set_power(int power)
+void Ressource:: Set_gold(int var)
 {
-    *m_power=power;
+    *m_gold=var;
+}
+void Ressource:: Set_power(int var)
+{
+    *m_power=var;
 }
 
 void Ressource::Set_influence(int influence)
