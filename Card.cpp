6e9470c058191e9dc6ef5468d20264ac844551a0
Mikhali <33858877+Mikhali@users.noreply.github.com>
2017-11-21 09:42:54 +0100

    Update Card.cpp
    get + set

--- Card.cpp
+++ Card.cpp
@@ -7,3 +7,33 @@
 //
 
 #include "Card.hpp"
+
+Card::Get_hand_status()
+{
+    return m_hand;
+}
+
+Card::Get_invoke_status()
+{
+    return m_invoke;
+}
+
+Card::Get_live_status()
+{
+    m_alive;
+}
+
+void Card::Set_hand_status(bool set)
+{
+    m_hand=set;
+}
+
+void Card::Set_invoke_status(bool set)
+{
+    m_invoke=set;
+}
+
+void Card::Set_live_status(bool set)
+{
+    m_alive=set;
+}
