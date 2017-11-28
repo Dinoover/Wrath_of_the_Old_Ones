30cf97456860ee40f6833c68ef40e5e9be9cfe33
Miletic Petar <miletic.petar.mp@gmail.com>
2017-11-28 09:30:23 +0100

    Add files via upload

---
+++ allegro_init.hpp
@@ -0,0 +1,19 @@
+#include "allegro.h"
+
+
+void allegro_initialisation()
+{
+    allegro_init();
+    install_keyboard();
+    install_mouse();
+
+    set_color_depth(desktop_color_depth());
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
+    {
+        allegro_message("prb gfx mode");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+
+    //show_mouse(screen);
+}
