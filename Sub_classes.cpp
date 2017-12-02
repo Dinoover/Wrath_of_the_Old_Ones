4e02a3d592d722b41ba03e7eb2dff5b02391d639
Lazovchik <33687555+Lazovchik@users.noreply.github.com>
2017-12-02 13:39:18 +0100

    Update Sub_classes.cpp

--- Sub_classes.cpp
+++ Sub_classes.cpp
@@ -15,10 +15,10 @@ Ressource:: Ressource()
 
 Ressource:: Ressource (int blood, int gold, int power, int influence)
 {
-    *m_blood=blood;
-    *m_gold=gold;
-    *m_power=power;
-    *m_influence=influence;
+    m_blood=blood;
+    m_gold=gold;
+    m_power=power;
+    m_influence=influence;
     
 }
 
@@ -29,38 +29,38 @@ Ressource:: ~Ressource()
 
  int Ressource:: Get_blood()
 {
-    return *m_blood;
+    return m_blood;
 }
  int Ressource:: Get_gold()
 {
-    return * m_gold;
+    return  m_gold;
 }
 int Ressource:: Get_power()
 {
-    return *m_power;
+    return m_power;
 }
 int Ressource::Get_influence()
 {
-    return *m_influence;
+    return m_influence;
 }
 
 
 void Ressource:: Set_blood(int var)
 {
-    *m_blood=var;
+    m_blood=var;
 }
 void Ressource:: Set_gold(int var)
 {
-    *m_gold=var;
+    m_gold=var;
 }
 void Ressource:: Set_power(int var)
 {
-    *m_power=var;
+    m_power=var;
 }
 
 void Ressource::Set_influence(int influence)
 {
-    *m_influence=influence;
+    m_influence=influence;
 }
 //Static_par
 
@@ -68,13 +68,13 @@ Static_Parameters:: Static_Parameters()
 {
     
 }
-Static_Parameters:: Static_Parameters(int * blood, int * gold, int * power,int* influence, int * damage, std::string name, int type, int number)
+Static_Parameters:: Static_Parameters(int  blood, int  gold, int power,int influence, int  damage, std::string name, int type, int number)
 {
-    *m_blood_coast=*blood;
-    *m_gold_coast=*gold;
-    *m_power_coast=*power;
-    *m_influence_coast=*influence;
-    *m_damage_value=*damage;
+    m_blood_coast=blood;
+    m_gold_coast=gold;
+    m_power_coast=power;
+    m_influence_coast=influence;
+    m_damage_value=damage;
     m_card_name=name;
     card_type=type;
     card_unique_number=number;
@@ -87,15 +87,43 @@ Static_Parameters:: ~Static_Parameters()
 
 int Static_Parameters:: Get_damage_value()
 {
-      return *m_damage_value;
+      return m_damage_value;
 }
 int Static_Parameters:: Get_card_number()
 {
     return card_unique_number;
 }
+
+int Static_Parameters:: Get_blood_coast()
+{
+    return m_blood_coast;
+}
+int Static_Parameters:: Get_gold_coast()
+{
+    return m_gold_coast;
+}
+int Static_Parameters:: Get_power_coast()
+{
+    return m_power_coast;
+}
+int Static_Parameters:: Get_influence_coast()
+{
+    return m_influence_coast;
+}
+
+std::string Static_Parameters:: Get_card_name()
+{
+    return m_card_name;
+}
+
+ int Static_Parameters:: Get_card_type()
+{
+    return card_type;
+}
+
 void Static_Parameters:: Set_damage_value(int var)
 {
-    *m_damage_value=var;
+    m_damage_value=var;
 }
 void Static_Parameters:: Set_card_number(int var)
 {
@@ -111,11 +139,11 @@ Sp_at_par:: Sp_at_par()
 
 Sp_at_par:: Sp_at_par (int blood, int gold, int power, int damage, int influence, std::string name)
 {
-    *m_blood_at_coast=blood;
-    *m_gold_at_coast=gold;
-    *m_power_at_coast=power;
-    *m_damage_value=damage;
-    *m_influence_at_coast=influence;
+    m_blood_at_coast=blood;
+    m_gold_at_coast=gold;
+    m_power_at_coast=power;
+    m_damage_value=damage;
+    m_influence_at_coast=influence;
      m_special_at_name=name;
 }
 
@@ -126,52 +154,52 @@ Sp_at_par:: ~Sp_at_par()
 
 int Sp_at_par:: Get_blood_at_coast()
 {
-    return *m_blood_at_coast;
+    return m_blood_at_coast;
 }
 int Sp_at_par:: Get_gold_at_coast()
 {
-    return *m_gold_at_coast;
+    return m_gold_at_coast;
 }
 int Sp_at_par:: Get_power_at_coast()
 {
-    return *m_power_at_coast;
+    return m_power_at_coast;
 }
 int Sp_at_par:: Get_influence_at_coast()
 {
-    return *m_influence_at_coast;
+    return m_influence_at_coast;
 }
 
 
 void Sp_at_par:: Set_blood_at_coast(int var)
 {
-    *m_blood_at_coast=var;
+    m_blood_at_coast=var;
 }
 void Sp_at_par:: Set_gold_at_coast(int var)
 {
-    *m_gold_at_coast=var;
+    m_gold_at_coast=var;
 }
 void Sp_at_par:: Set_power_at_coast(int var)
 {
-    *m_power_at_coast=var;
+    m_power_at_coast=var;
 }
 void Sp_at_par:: Set_influence_at_coast(int var)
 {
-    *m_influence_at_coast=var;
+    m_influence_at_coast=var;
 }
 Variant_Parametrs:: Variant_Parametrs()
 {
     
 }
-Variant_Parametrs:: Variant_Parametrs(int * force, int* HP)
-{   if (force!=NULL && HP!=NULL)
+Variant_Parametrs:: Variant_Parametrs(int force, int HP)
+{   if (force!=0 && HP!=0)
     {
-    *m_force=*force;
-    *m_HP=*HP;
+    m_force=force;
+    m_HP=HP;
     }
     else
     {
-        m_HP=NULL;
-        m_force=NULL;
+        m_HP=0;
+        m_force=0;
     }
     
     
@@ -184,20 +212,20 @@ Variant_Parametrs:: ~Variant_Parametrs()
 
 int Variant_Parametrs:: Get_force()
 {
-    return *m_force;
+    return m_force;
 }
 int Variant_Parametrs:: Get_HP()
 {
-    return *m_HP;
+    return m_HP;
 }
 
 void Variant_Parametrs:: Set_force(int force)
 {
-    *m_force=force;
+    m_force=force;
 }
 void Variant_Parametrs:: Set_HP(int HP)
 {
-    *m_HP=HP;
+    m_HP=HP;
 }
 
 
