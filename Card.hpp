bf27ef0358051ffff7ff6134f91ab2ab92c2b9b5
Lazovchik <33687555+Lazovchik@users.noreply.github.com>
2017-12-02 13:38:56 +0100

    Update Card.hpp

--- Card.hpp
+++ Card.hpp
@@ -7,38 +7,52 @@
 //
 
 #ifndef Card_hpp
-#define Card_hpp
-
-#include <iostream>
+#include<fstream>
 #include "Sub_classes.hpp"
+
 
 class Card
 {
 private:
-    bool m_hand;
+    bool m_deck;
     bool m_invoked;
     bool m_alive;
     
     Ressource* m_ressources;//pour les carte-terrain
-    Static_Parameters m_s_parametrs;//nom, image, cout etc..
+    Static_Parameters* m_s_parametrs;//nom, image, cout etc..
     Variant_Parametrs* m_v_parametres;//la vie, la force
     Sp_at_par * m_sp_at_parameter;//attaque speciale
     
     //Accees au tableau des energies A FAIRE
     
 public:
-    Card();
+     Card( std::ifstream &file);
+     Card();
     ~Card();
     
-    bool Get_hand_status();
+    bool Get_deck_status();
     bool Get_invoke_status();
     bool Get_live_status();
     
-    void Set_hand_status (bool var);
+    void Set_deck_status (bool var);
     void Set_invoke_status (bool var);
     void Set_live_status (bool var);
-
     
+    
+    
+    
+    int Get_ressource(int var);
+    
+
+    int  Get_s_parameter(int var);
+    
+    
+    std::string Get_NAME()
+    {
+        return this->m_s_parametrs->Get_card_name();
+    }
+
+    //virtual void Use_Ability(Card* carta);
     
     
 };
