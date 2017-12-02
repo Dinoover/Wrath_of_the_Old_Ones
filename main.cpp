af033239db7aefec2fb44e5fecf0a36d96aa13eb
Lazovchik <33687555+Lazovchik@users.noreply.github.com>
2017-12-02 13:37:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
-#include <iostream>
+
+//  Card_Old_Gods_game
+
+
+
+
+
+
+
+#include "Card.hpp"
+#include <vector>
+
 
 int main(int argc, const char * argv[]) {
-
     
+    std::vector<Card*> cards_table;
+    int cards_n(0);
+    std::string line;
+    std::ifstream myfile ("Cards.txt");
+    if (myfile.is_open())
+    {
+        myfile>>cards_n;
+        for(int i=0;i<cards_n;i++)
+        {
+            
+            cards_table.push_back(new Card (myfile));
+            //std::cout<< cards_table[i]->Get_s_parameter(1)<<cards_table[i]->Get_NAME();
+        }
+      
+        myfile.close();
+    }
     
+    else std::cout << "Unable to open file";
+    
+    for(int i=0;i<cards_table.size();i++)
+    {
+        delete cards_table[i];
+        
+    }
     
     return 0;
 }
