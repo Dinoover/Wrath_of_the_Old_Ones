a1540acb00e2df607b561654e686269c3ac8fcc3
Mikhali <33858877+Mikhali@users.noreply.github.com>
2017-12-03 18:20:43 +0100

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <stack>
+#include "Card.hpp"
 
 class Player
 {
@@ -9,7 +11,7 @@ class Player
     std::string m_nom;
     int* m_cp; //money for the store (combat point)
     std::stack<int> m_deck;
-    std::vector <card*>* collection;
+    std::vector <Card*> collection;
 
   public:
 
@@ -20,8 +22,9 @@ class Player
     void Set_deck(int);
     void Set_name(std::string);
     void Set_cp(int);
-    void ouvrir_booster(card*);
+    void ouvrir_booster(Card*);
 
     std::string Get_name();
     int Get_cp();
 };
+
