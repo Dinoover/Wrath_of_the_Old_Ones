6d2e5339166c7be87ab789afa3f7105e0d5e77c5
Lazovchik <33687555+Lazovchik@users.noreply.github.com>
2017-12-03 23:03:32 +0100

    Update Sub_classes.hpp

--- Sub_classes.hpp
+++ Sub_classes.hpp
@@ -105,7 +105,7 @@ protected:
     bool m_deck;
     bool m_invoked;
     bool m_alive;
-  
+    int m_card_deck_number;
 public:
     Variant_Parametrs();
     Variant_Parametrs(int  m_force, int m_HP);
@@ -128,5 +128,9 @@ public:
     void Set_live_status (bool var);
     
 };
+
+
+
+
 
 #endif /* Sub_classes_hpp */
